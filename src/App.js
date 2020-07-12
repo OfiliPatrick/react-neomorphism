98e8154e2e47a7c0c21b615309bf9b7ab5a45e8f
Patrick Ofili <patrickofili16@gmail.com>
2020-07-12 03:30:09 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Contact from './components/Contact'
+
+const App = () => {
+  return (   
+    <div>
+      <Contact/> 
+    </div>
+  );
+}
+
+export default App;
+
+  
---
+++ src/components/Contact.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1 className="contact">Contact Me</h1>
+      <div className="socials">
+        <a href="#">
+          <i className="fab fa-whatsapp"></i>
+        </a>
+        <a href="#">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a href="#">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a href="#">
+          <i className="fab fa-instagram"></i>
+        </a>
+        <a href="#">
+          <i className="fab fa-github"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
